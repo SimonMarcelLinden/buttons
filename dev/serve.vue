3cc2bfc1642882571713eca3e84f13c4c0ab211b
SimonMarcelLinden <info@snerve.de>
2021-07-26 22:23:48 +0200

    Initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { ButtonsSample } from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  // components: {
+  //  ButtonsSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <buttons-sample />
+  </div>
+</template>
